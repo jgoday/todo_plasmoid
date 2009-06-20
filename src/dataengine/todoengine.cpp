cd4a2816aa57f64e223cb5af47deff65012a1a98
Javier Goday <jgoday@jgoday-home.(none)>
2009-06-20 21:09:04 +0200

    Fix license version (GPL v2) in source files

--- src/dataengine/todoengine.cpp
+++ src/dataengine/todoengine.cpp
@@ -1,7 +1,7 @@
 /*
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
+ *   the Free Software Foundation, either version 2 of the License, or
  *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
@@ -14,7 +14,6 @@
  *
  *   Copyright (C) 2008 by Javier Goday <jgoday@gmail.com>
  */
-
 #include "todoengine.h"
 #include "korganizerutil.h"
 
